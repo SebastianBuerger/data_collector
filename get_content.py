7049c1c606f7f7d9ca26e4c30d896b2d4e9d29d7
Sebastian Bürger <buerger@surgitaix.com>
2017-04-06 13:20:11 +0200

    started adding parsing capability lxml

--- get_content.py
+++ get_content.py
@@ -1,4 +1,10 @@
 import urllib2
+# lxml/xpath usage: https://www.w3schools.com/xml/xpath_syntax.asp 
+# nomenclature of xml: http://archive.oreilly.com/pub/a/perl/excerpts/system-admin-with-perl/ten-minute-xpath-utorial.html
+
+import lxml.html
+from lxml import etree
+
 
 req = urllib2.Request('http://www.wg-gesucht.de/wg-zimmer-in-Aachen.1.0.1.0.html')
 try:
@@ -14,6 +20,34 @@ except urllib2.URLError as e:
     print 'a not HTTP specific error'
 else:
     # 200
-    body = resp.read()
-    print body
+    content = resp.read()
+    
+
+    
+    doc = lxml.html.fromstring(content)
+    
+    watch_els = []
+
+    links = doc.xpath('//a[@href]')
+
+    #els = doc.xpath('//a')
+#     for el in els:
+#         text = el.xpath("//text()")
+#         href = el.xpath("//@href")
+#         #check text and href arrays are not empty...
+#         if len(href) <= 0 or len(text) <= 0:
+#             #empty text/href, skip.
+#             continue
+#     
+#         text = text[0]
+#         href = href[0]
+#         print text
+#         print href
+#         
+#         if "/watch?" in href:
+#             #do something with a youtube video link...
+#             watch_els.append((text, href))
+    
+    
+    print links[0].xpath("//@href")
 
